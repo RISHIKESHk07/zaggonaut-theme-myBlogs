c9554843cb82ac01a2798643df1f543cc2fc39d3
RISHIKESHk07 <117551967+RISHIKESHk07@users.noreply.github.com>
2025-07-13 19:57:04 +0530

    Update variables.ts

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -32,7 +32,7 @@ export const GLOBAL = {
   projectLongDescription: "All of my projects, including both frontend and full-stack applications.",
 
   // Profile image
-  profileImage: "images/gitubpp.png",
+  profileImage: "gitubpp.png",
 
   // Menu items
   menu: {
